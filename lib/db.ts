fca548030047bf9fe304317e43622512847436b9
tfarrecys <tfarrerascys@gmail.com>
2025-05-30 02:54:25 -0300

    arregla error no aparece soli a admin2

--- lib/db.ts
+++ lib/db.ts
@@ -5,7 +5,11 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-export const db = globalForPrisma.prisma ?? new PrismaClient()
+export const db =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["query"],
+  })
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db
 
