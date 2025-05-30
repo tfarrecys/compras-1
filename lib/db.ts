fe36ebe3695aff6fbefe128a298bfdf3071d4568
tfarrecys <tfarrerascys@gmail.com>
2025-05-30 02:57:07 -0300

    arregla error no aparece soli a admin2

--- lib/db.ts
+++ lib/db.ts
@@ -1,17 +1,13 @@
 import { PrismaClient } from "@prisma/client"
 import { neon } from "@neondatabase/serverless"
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
+declare global {
+  var prisma: PrismaClient | undefined
 }
 
-export const db =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ["query"],
-  })
+const db = global.prisma || new PrismaClient()
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db
+if (process.env.NODE_ENV !== "production") global.prisma = db
 
 const sql = neon(process.env.DATABASE_URL!)
 
